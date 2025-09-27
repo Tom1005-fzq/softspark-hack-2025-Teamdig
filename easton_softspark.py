548176ccd9cb8c9166d0257a7ce3fecb1275795c
Easton-Lai <3169045158@qq.com>
2025-09-27 11:01:42 +1000

    final (#5)
    * remove hello.py
    
    * add easton_softspark.pv
    
    * update: add greeting message
    
    * update: add greeting message
    
    * delete index.html, update easton_softspark.py
    
    * update easton_softspark.py，delete index.html
    
    ---------
    
    Co-authored-by: Easton_Lai <easton_lai@Easton-LaideMacBook-Air.local>

--- easton_softspark.py
+++ easton_softspark.py
@@ -1,1 +1,23 @@
-print('hello,everybody')
+import random
+
+def play_game():
+
+    target = random.randint(1, 100)
+    attempts = 0
+
+    print('I already choosed a number! Come and Guess!')
+
+    while True:
+        guess = int(input("Enter the number you guessed:"))
+        attempts += 1
+
+        if guess < target:
+            print("Too Low")
+        elif guess > target:
+            print("Too High")
+        else:
+            print(f"Congratulation! The correct is {target}")
+            break
+
+if __name__ == "__main__":
+    play_game()
