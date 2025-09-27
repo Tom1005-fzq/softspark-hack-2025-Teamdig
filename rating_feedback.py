a31d1162ebe07d976fa15c3ea6258094fc51682d
YuruihanFeng <f410311930@outlook.com>
2025-09-27 11:12:22 +1000

    feat(rating): add rating_feedback.py with evaluation & feedback

---
+++ rating_feedback.py
@@ -0,0 +1,7 @@
+# 评价
+if attempts <= 5:
+    print(f"You guessed it {attempts} in total，Review：Excellent！")
+elif attempts <= 8:
+    print(f"You guessed it {attempts} in total，Review：Good！")
+else:
+    print(f"You guessed it {attempts} in total，Review：Try again！")
